780e701517065ea27d4be52671d4d57be5bcea57
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-07-21 20:13:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# data analysis and wrangling
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# In[3]:
+
+
+df = pd.read_csv(r"C:\Users\srikanthve\Desktop\stylereco.csv")
+
+
+# In[4]:
+
+
+head = df.head()
+
+
+# In[ ]:
+st.write(head)
+
+
+
