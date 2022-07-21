2e5ffab04a6d5fa750031a89700bef7d2cf00ffc
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2022-07-21 20:21:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# data analysis and wrangling
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# In[3]:
+
+
+# In[4]:
+
+
+st.title("App Engine sample app")
+
+
+# In[ ]:
+
+
+
+
